b3773db7901376f1f35dd5a70b931ff357046d38
Daniel Weaver <danielw@fb.com>
2018-08-28 17:27:22 -0700

    Formatting guidance from DomT4 on the pull request

--- Formula/bloaty.rb
+++ Formula/bloaty.rb
@@ -3,11 +3,12 @@ class Bloaty < Formula
   homepage "https://github.com/google/bloaty"
   url "https://github.com/google/bloaty/releases/download/v1.0/bloaty-1.0.tar.bz2"
   sha256 "e1cf9830ba6c455218fdb50e7a8554ff256da749878acfaf77c032140d7ddde0"
+
   depends_on "cmake" => :build
 
   def install
     system "cmake", ".", *std_cmake_args
-    system "make", "-j#{ENV.make_jobs}"
+    system "make"
     bin.install buildpath/"bloaty"
   end
 
